950e49e38c698b56afcab34138e039e8f305bafb
deveripon <developershahadathussainripon@gmail.com>
2024-01-07 22:33:31 +0600

    initialize statrter template for react with tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+    return (
+        <h1 className="text-3xl m-auto text-center flex justify-center items-center h-screen font-bold text-cyan-800">
+            React Template with Tailwind Css
+        </h1>
+    );
+}
